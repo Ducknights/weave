0d81368ae1578b3e1a3066e9f78a991667316fd1
野生的鸭子 <2897662424@qq.com>
2026-02-22 21:17:23 +0800

    feat(common): 新增用户交互相关缓存键常量

--- my-common-boot-starter/src/main/java/org/example/strings/CacheKey.java
+++ my-common-boot-starter/src/main/java/org/example/strings/CacheKey.java
@@ -17,6 +17,30 @@ public class CacheKey {
      * 活动缓存区域
      */
     public static final String ACTIVITY_AREA = "Activity";
+    /**
+     * 用户点赞缓存区域
+     */
+    public static final String USER_LIKED_POSTS = "user:liked:posts";
+    /**
+     * 用户收藏帖子缓存区域
+     */
+    public static final String USER_COLLECTED_POSTS = "user:collected:posts";
+    /**
+     * 用户分享帖子缓存区域
+     */
+    public static final String USER_SHARED_POSTS = "user:shared:posts";
+    /**
+     * 用户关注用户缓存区域
+     */
+    public static final String USER_FOLLOWERS = "user:followers";
+    /**
+     * 用户屏蔽用户缓存区域
+     */
+    public static final String USER_MUTED_USERS = "user:muted:users";
+    /**
+     * 用户拉黑用户缓存区域
+     */
+    public static final String USER_BLOCKED_USERS = "user:blocked:users";
 
     public static String buildCacheKey(String area, Object identifier) {
         return area + KEY_SEPARATOR + identifier;
