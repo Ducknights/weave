aaa0f86d8b10b6f415a4c606c275d4d7b752aee0
野生的鸭子 <2897662424@qq.com>
2026-05-15 08:50:12 +0800

    feat(auth-service): 新增UserDto类支持用户角色信息

---
+++ services/auth-service/src/main/java/org/example/dto/UserDto.java
@@ -0,0 +1,17 @@
+package org.example.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class UserDto extends UserBriefDto{
+    private List<String> roles;
+
+    public UserDto(Long userId, String name, String avatar, List<String> roleNames) {
+        super(userId, name, avatar);
+        this.roles = roleNames;
+    }
+}
