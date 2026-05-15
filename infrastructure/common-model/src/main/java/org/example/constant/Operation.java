0a5ff0b1e758fcc5fdd896222fbd20b4ca8bd91e
野生的鸭子 <2897662424@qq.com>
2026-05-15 09:08:51 +0800

    feat(common-model): 新增PostSyncMessage及Operation常量

---
+++ infrastructure/common-model/src/main/java/org/example/constant/Operation.java
@@ -0,0 +1,8 @@
+package org.example.constant;
+
+public class Operation {
+    public static final String CREATE = "create";
+    public static final String UPDATE = "update";
+    public static final String DELETE = "delete";
+    public static final String QUERY = "query";
+}
