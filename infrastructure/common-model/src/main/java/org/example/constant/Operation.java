d051acb9a73f000c4771b9dec93171f833b85dac
野生的鸭子 <2897662424@qq.com>
2026-05-24 12:17:40 +0800

    refactor(common-model): 删除无用的 Operation 常量类

--- infrastructure/common-model/src/main/java/org/example/constant/Operation.java
+++
@@ -1,7 +0,0 @@
-package org.example.constant;
-
-public class Operation {
-    public static final String CREATE = "create";
-    public static final String UPDATE = "update";
-    public static final String DELETE = "delete";
-}
