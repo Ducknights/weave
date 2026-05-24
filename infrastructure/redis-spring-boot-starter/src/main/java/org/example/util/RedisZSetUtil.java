42886ea1bdeb6dcafeebffdb61ec14b5faf48a3a
野生的鸭子 <2897662424@qq.com>
2026-05-24 18:54:49 +0800

    refactor(redis): 重命名RedisSortedSetUtil为RedisZSetUtil，调整用户收藏缓存键命名

--- infrastructure/redis-spring-boot-starter/src/main/java/org/example/util/RedisZSetUtil.java
+++ infrastructure/redis-spring-boot-starter/src/main/java/org/example/util/RedisZSetUtil.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 import java.util.Set;
 
 @Component
-public class RedisSortedSetUtil {
+public class RedisZSetUtil {
 
     @Resource
     private RedisTemplate<String, Object> redisTemplate;
