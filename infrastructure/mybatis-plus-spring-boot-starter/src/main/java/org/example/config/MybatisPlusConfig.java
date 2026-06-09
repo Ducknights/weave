3a9282be704f2abcdb6df138e70b44a5986f929b
野生的鸭子 <2897662424@qq.com>
2026-06-10 05:24:46 +0800

    refactor(mybatis-plus): 移除未使用的OptimisticLockerInnerInterceptor导入

--- infrastructure/mybatis-plus-spring-boot-starter/src/main/java/org/example/config/MybatisPlusConfig.java
+++ infrastructure/mybatis-plus-spring-boot-starter/src/main/java/org/example/config/MybatisPlusConfig.java
@@ -3,7 +3,6 @@ package org.example.config;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
