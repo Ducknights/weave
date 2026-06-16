044fdca72290837d35e21ec7636e5bde1b9a2366
野生的鸭子 <2897662424@qq.com>
2026-06-17 01:08:06 +0800

    refactor(post): 移除Post实体中未使用的TableLogic导入

--- services/post-service/src/main/java/org/example/model/entity/Post.java
+++ services/post-service/src/main/java/org/example/model/entity/Post.java
@@ -2,7 +2,6 @@ package org.example.model.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Builder;
 import lombok.Data;
