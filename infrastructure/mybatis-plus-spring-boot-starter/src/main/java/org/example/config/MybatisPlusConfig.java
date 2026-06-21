22c5eabd55ef7245f10690d3313baf33ecf13874
野生的鸭子 <2897662424@qq.com>
2026-06-22 04:29:38 +0800

    feat(mybatis): 添加乐观锁插件支持

--- infrastructure/mybatis-plus-spring-boot-starter/src/main/java/org/example/config/MybatisPlusConfig.java
+++ infrastructure/mybatis-plus-spring-boot-starter/src/main/java/org/example/config/MybatisPlusConfig.java
@@ -2,6 +2,7 @@ package org.example.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +15,8 @@ public class MybatisPlusConfig {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         // 分页插件
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        // 乐观锁
+        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
         return interceptor;
     }
 }
